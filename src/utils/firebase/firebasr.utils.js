36c0aeeea869ad536758453c16d6fdc9ca010840
Robert B <robertb@lifesciencepublishers.com>
2022-04-15 14:18:13 -0600

    Add Firebase and setup user model

--- src/utils/firebase/firebasr.utils.js
+++ src/utils/firebase/firebasr.utils.js
@@ -1,4 +1,3 @@
-import { async } from '@firebase/util';
 import { initializeApp } from 'firebase/app';
 import { 
     getAuth, 
@@ -44,10 +43,7 @@ const firebaseConfig = {
 
   export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
-    console.log(userDocRef);
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
 
     if(!userSnapshot.exists()){
         const { displayName, email } = userAuth;
