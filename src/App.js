c5596718ad63c0f0d5cb9e316588f8e51250d5da
Robert B <robertb@lifesciencepublishers.com>
2022-05-10 16:56:19 -0600

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import {Routes, Route} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+
+import {onAuthStateChangedListener, createUserDocumentFromAuth} from './utils/firebase/firebasr.utils';
+import {setCurrentUser} from './store/user/user.action';
 
 import Home from './routes/home/home.component';
 import Navbar from './routes/navbar/Navbar.component';
@@ -7,6 +13,20 @@ import Shop from './routes/shop/Shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChangedListener((user)=>{
+        if(user){
+             createUserDocumentFromAuth(user)
+        }
+        dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+ }, [dispatch]);
+
   return (
     <Routes>
       <Route path='/' element={<Navbar />}>
